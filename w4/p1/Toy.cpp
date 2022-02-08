6b3ab10ab1e65e484b99ef3a9a6942f5788c3c48
Rohan Kaicker <rkaicker@myseneca.ca>
2022-02-07 19:52:36 -0500

    finalized part 1

--- w4/p1/Toy.cpp
+++ w4/p1/Toy.cpp
@@ -11,32 +11,56 @@
 
 namespace sdds{
     Toy::Toy(const std::string& toy){
-        int begPos = 0;
         int endPos = 0;
-        std::string subStr{};
+        std::string tempStr = toy;
         std::string delim = ":";
 
-        for (int i = 0; i < 3; i++){
-            endPos = toy.find(delim,begPos);
+        for (int i = 0; i < 4; i++){
+            endPos = tempStr.find(delim);
             switch(i){
                 case 0:
-                    m_orderID = std::stoi(toy.substr(begPos,endPos));
+                    m_orderID = std::stoi(tempStr.substr(0,endPos));
+                    std::cout << m_orderID;
                     break;
                 case 1:
-                    m_name = toy.substr(begPos,endPos);
+                    m_name = tempStr.substr(0,endPos);
+                    std::cout << m_name;
                     break;
                 case 2:
-                    m_numItems = std::stoi(toy.substr(begPos,endPos));
+                    m_numItems = std::stoi(tempStr.substr(0,endPos));
+                    std::cout << m_numItems;
                     break;
                 case 3:
-                    m_price = std::stod(toy.substr(begPos,endPos));
+                    int finalPos = tempStr.find('\n');
+                    m_price = std::stod(tempStr.substr(0,finalPos));
+                    std::cout << m_price;
                     break;
             }
-            begPos = endPos + 1;
+            tempStr.erase(0,endPos + 1);
         }
     }
 
     std::ostream& operator<<(std::ostream& os, const Toy& T){
-
+        double subTot = T.m_price * T.m_numItems;
+        double tax = T.m_tax * subTot;
+        double total = tax + subTot;
+        os.setf(std::ios::fixed);
+        os.precision(2);
+        os << "Toy";
+        os.width(8);
+        os << T.m_orderID << ":";
+        os.width(18);
+        os << T.m_name;
+        os.width(3);
+        os << T.m_numItems << " items";
+        os.width(8);
+        os << T.m_price << "/item subtotal:";
+        os.width(7);
+        os << subTot << " tax:";
+        os.width(6);
+        os << tax << " total:";
+        os.width(7);
+        os << total << std::endl;
+        return os;
     }
 }
