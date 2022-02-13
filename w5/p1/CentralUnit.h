6cfbaba3137ead270ebc1938ba69316d759eae2e
Rohan Kaicker <rkaicker@myseneca.ca>
2022-02-12 20:22:30 -0500

    initial implementation of central unit module

--- w5/p1/CentralUnit.h
+++ w5/p1/CentralUnit.h
@@ -8,9 +8,26 @@
 */
 #ifndef CENTRALUNIT_H
 #define CENTRALUNIT_H
-
+#include "Job.h"
 namespace sdds{
-    
+    template <typename T>
+    class CentralUnit{
+        std::string m_type{};
+        T** m_items{nullptr};
+        Job* m_jobs[4];
+        size_t m_size{};
+        size_t m_count{};
+    public:
+        std::ostream& log{std::cout};
+        CentralUnit(std::string type, char* fileName);
+        CentralUnit(const CentralUnit& src)=delete;
+        CentralUnit& operator=(const CentralUnit& src)=delete;
+
+        CentralUnit& operator+=(std::string jobTitle);
+        void run();
+        bool has_jobs()const;
+        int get_available_units()const;
+    };
 }
 
 #endif
