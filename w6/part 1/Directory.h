2040fb907a5853732b82c9c63b04f320e4b31ccf
Rohan Kaicker <rkaicker@myseneca.ca>
2022-03-06 15:18:54 -0500

    initial implementation of directory module

--- w6/part 1/Directory.h
+++ w6/part 1/Directory.h
@@ -11,22 +11,37 @@
 #define SDDS_DIRECTORY_H
 
 #include <vector>
+#include "Resource.h"
 
 namespace sdds{
     class Directory : public Resource {
         std::vector<Resource*> m_contents{};
 
     public:
+        // Custom Constructor
+        Directory(std::string name){m_name = (name + "/");};
+
+
+        // Rule of 5 - deleted copy/ move operations
+        Directory(const Directory& src) = delete;
+        Directory(Directory&& src) = delete;
+        Directory& operator=(const Directory& src) = delete;
+        Directory& operator=(Directory&& src) = delete;
+        ~Directory();
+
+        // Modifier
         void update_parent_path(const std::string& path);
-        NodeType type() const;
-        std::string path() const;
-        std::string name() const;
-        int count() const;
+
+        // Queries
+        NodeType type() const {return NodeType::DIR;};
+        std::string path() const{return (m_parent_path + m_name);};
+        std::string name() const{return m_name;};
+        int count() const{return m_contents.size();};
         size_t size() const;
 
         Directory& operator+=(Resource* newRes);
-        Resource* find(const std::string&, const std::vector<OpFlags>&);
-        
+        Resource* find(const std::string&, const std::vector<OpFlags>& = {});
+
 
     };
 }
