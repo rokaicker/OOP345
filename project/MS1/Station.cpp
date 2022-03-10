164ddd2413ae2a82b9e9513963bb095758ad4630
Rohan Kaicker <rkaicker@myseneca.ca>
2022-03-10 12:42:47 -0500

    initial implementation of station module

--- project/MS1/Station.cpp
+++ project/MS1/Station.cpp
@@ -5,3 +5,42 @@
 //
 // I confirm that I am the only author of this file
 //   and the content was created entirely by me.
+
+#include "Station.h"
+#include "Utilities.h"
+
+namespace sdds{
+    Station::Station(const std::string& inputLine){
+        // delimiter used by utilities object already set by client code
+        m_stationID = id_generator++;
+        Utilities util{};
+        bool more = true; 
+        size_t pos = 0u;
+
+        // Extracting name
+        m_itemName = util.extractToken(inputLine, pos, more);
+
+        //Updating station::m_widthField
+        if (m_widthField < util.getFieldWidth()) m_widthField = util.getFieldWidth();
+
+        // Extracting serial number, need to convert string to integer
+        m_nextSerial = std::stoi(util.extractToken(inputLine, pos, more));
+
+        // Extracting items in stock, need to convert string to integer then cast size_t
+        m_itemsInStock = static_cast<size_t>(std::stoi(util.extractToken(inputLine, pos, more)));
+
+        // Extracting Description
+        m_stationDescription = util.extractToken(inputLine, pos, more);
+    }
+
+    void Station::display(std::ostream& os, bool full) const{
+        os << std::setw(3) << std::right << std::setfill('0') << m_stationID << " | ";
+        os << std::setw(m_widthField) << std::left << m_itemName << " | ";
+        os << std::setw(6) << std::right << std::setfill('0') << m_nextSerial << " | ";
+        if (full){
+            os << std::setw(4) << std::right << m_itemsInStock << " | ";
+            os << std::left << m_stationDescription;
+        }
+        os << std::endl;
+    }
+}
