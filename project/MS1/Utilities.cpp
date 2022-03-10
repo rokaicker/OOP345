537a704d73fde9099d7d21b8bcf5b9999b07f7fe
Rohan Kaicker <rkaicker@myseneca.ca>
2022-03-10 14:01:41 -0500

    corrected logic for extractToken()

--- project/MS1/Utilities.cpp
+++ project/MS1/Utilities.cpp
@@ -15,16 +15,30 @@ namespace sdds{
     char Utilities::m_delimiter{};
 
     std::string Utilities::extractToken(const std::string &str, size_t &next_pos, bool &more){
+        
+        size_t delimPos = str.find(m_delimiter, next_pos);
+        if (delimPos == std::string::npos || delimPos == next_pos) {
+            more = false;
+        }
+        else {
+            more = true;
+        }
+
+        std::string returnStr{};
+
+        if (delimPos == std::string::npos) {
+            returnStr = str.substr(next_pos);
+        }
+        else {
+            returnStr = str.substr(next_pos, delimPos - next_pos);
+        }
         // Exception thrown if delimiter found at next_pos
-        if (str.at(next_pos) == m_delimiter){
+        if (delimPos == next_pos) {
             throw std::string("Delimiter found at next position");
         }
 
-        size_t delimPos = str.find_first_of(str, next_pos);
-        more = delimPos != std::string::npos;   // more = true if delim was found in string
-        std::string returnStr = str.substr(next_pos, delimPos - next_pos);
         returnStr = trim(returnStr);
-        std::cout << returnStr;
+
         if (returnStr.size() > m_widthField){
             m_widthField = returnStr.size();
         }
