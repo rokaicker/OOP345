22f03d3288e8957baa7642c887747f0361cc2324
Rohan Kaicker <rkaicker@myseneca.ca>
2022-03-16 13:18:01 -0400

    quick commit of p1 files

---
+++ w7/part 2/CovidCollection.h
@@ -0,0 +1,60 @@
+/*
+* Name: Rohan Kaicker
+* Seneca Email: rkaicker@myseneca.ca
+* Seneca Student ID: 119070217
+* Date: March 20, 2022
+*
+* I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+*/
+#ifndef SDDS_COVIDCOLLECTION_H
+#define SDDS_COVIDCOLLECTION_H
+
+#include <string>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+
+namespace sdds
+{
+    struct Covid
+    {
+        std::string m_country{};
+        std::string m_city{};
+        std::string m_variant{};
+        int m_numCases{};
+        int m_year{};
+        int m_numDeaths{};
+    };
+
+    class CovidCollection
+    {
+        std::vector<Covid> m_covid{};
+        /*
+        ltrim(), rtrim(), and trim() work in conjunction to remove leading and trailing spaces from a string that has been passed to them. 
+        If a string is passed to these functinos that only contains "spaces", an empty string will be returned.
+        */
+        std::string ltrim(const std::string &str){
+            size_t start = str.find_first_not_of(" ");
+            return (start == std::string::npos) ? "" : str.substr(start);
+        }
+
+        std::string rtrim(const std::string &str){
+            size_t end = str.find_last_not_of(" ");
+            return (end == std::string::npos) ? "" : str.substr(0,end + 1);
+        }
+
+        std::string trim(const std::string &str){
+            return rtrim(ltrim(str));
+        }
+
+    public:
+        CovidCollection(const std::string fileName);
+        void display(std::ostream& os) const;
+        friend std::ostream& operator<<(std::ostream& os, const Covid& theCovid);
+    };
+}
+
+
+#endif
