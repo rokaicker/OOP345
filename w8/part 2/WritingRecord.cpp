37215ab8e00c7cdf973f798900b8ebd8bfce1ed1
Rohan Kaicker <rkaicker@myseneca.ca>
2022-03-18 14:53:00 -0400

    added my information to the top of all files

--- w8/part 2/WritingRecord.cpp
+++ w8/part 2/WritingRecord.cpp
@@ -1,3 +1,11 @@
+/*
+* Name: Rohan Kaicker
+* Seneca Email: rkaicker@myseneca.ca
+* Seneca Student ID: 119070217
+* Date: March 18, 2022
+*
+* I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+*/
 #include "GeneratingList.h"
 #include "EmpProfile.h"
 #include "WritingRecord.h"
