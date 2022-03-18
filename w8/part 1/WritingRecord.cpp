afec80188e6a23973fe0330d864743303a615145
Rohan Kaicker <rkaicker@myseneca.ca>
2022-03-18 13:22:20 -0400

    implemented writeRaw() function

--- w8/part 1/WritingRecord.cpp
+++ w8/part 1/WritingRecord.cpp
@@ -7,11 +7,28 @@ using namespace std;
 namespace sdds {
 	GeneratingList<EmployeeWage> writeRaw(const GeneratingList<Employee>& emp, const GeneratingList<Salary>& sal) {
 		GeneratingList<EmployeeWage> activeEmp;
-		
-		// TODO: Add your code here to build a list of employees
-		//         using raw pointers
-		
-		
+		// Validate salary using rangeValidator()
+		// Validate SIN using luhn()
+		// Recall that generatinglist objects have had index operator overloaded and size() returns the size of the vector member
+
+		for (size_t i = 0; i < emp.size(); i++) {
+			for (size_t j = 0; i < sal.size(); i++){
+				if (emp[i].id == sal[i].id){
+					// If same id exists, create new dynamic employee wage
+					EmployeeWage* temp = new EmployeeWage(emp[i].name, sal[i].salary);
+					// Validation of EmployeeWage based on salary and SIN(ID)
+					temp->rangeValidator();	// This returns nothing, but if validation fails an exception will be thrown and the below code won't be executed
+					if(activeEmp.luhn(emp[i].id)){
+						activeEmp += temp;
+					} else {
+						throw std::string ("The employee SIN did not pass the Luhn algorithm");
+					}
+					delete temp;
+					temp = nullptr;
+					break;	// break down to get out of the inner for loop and move onto the next iteration in the outer for loop
+				}
+			}
+		}
 		return activeEmp;
 	}
 }
