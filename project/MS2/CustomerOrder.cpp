4d3cdab563d915b332fd581085fcce8607b8c66d
Rohan Kaicker <rkaicker@myseneca.ca>
2022-03-19 23:42:48 -0400

    initial commit of CustomerOrder module

---
+++ project/MS2/CustomerOrder.cpp
@@ -0,0 +1,16 @@
+// Name: Rohan Kaicker
+// Seneca Student ID: 119070217
+// Seneca email: rkaicker@myseneca.ca
+// Date of completion: March , 2022
+//
+// I confirm that I am the only author of this file
+//   and the content was created entirely by me.
+
+#include "CustomerOrder.h"
+
+namespace sdds
+{
+    
+
+
+}
---
+++ project/MS2/CustomerOrder.h
@@ -0,0 +1,19 @@
+// Name: Rohan Kaicker
+// Seneca Student ID: 119070217
+// Seneca email: rkaicker@myseneca.ca
+// Date of completion: March , 2022
+//
+// I confirm that I am the only author of this file
+//   and the content was created entirely by me.
+
+#ifndef SDDS_CUSTOMERORDER_H
+#define SDDS_CUSTOMERORDER_H
+
+namespace sdds
+{
+
+
+
+}
+
+#endif // SDDS_CUSTOMERORDER_H
