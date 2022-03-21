0bf73621df85d68517f8bc8dcb1f0c143c721352
Rohan Kaicker <rkaicker@myseneca.ca>
2022-03-20 23:09:53 -0400

    added global variable declaration

--- project/MS3/Workstation.h
+++ project/MS3/Workstation.h
@@ -5,3 +5,25 @@
 //
 // I confirm that I am the only author of this file
 //   and the content was created entirely by me.
+
+#ifndef SDDS_WORKSTATION_H
+#define SDDS_WORKSTATION_H
+#include <deque>
+#include "Station.h"
+#include "CustomerOrder.h"
+namespace sdds
+{
+    // Global Variable Declaration
+    extern std::deque<CustomerOrder> g_pending;
+    extern std::deque<CustomerOrder> g_completed;
+    extern std::deque<CustomerOrder> g_incomplete;
+
+    class Workstation : public Station
+    {
+        
+    };
+
+
+}
+
+#endif // SDDS_WORKSTATION_H
