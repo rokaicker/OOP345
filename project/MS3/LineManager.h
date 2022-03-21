32e8e6af1fb4769958d144d5c87fb270ee2c4b86
Rohan Kaicker <rkaicker@myseneca.ca>
2022-03-21 10:48:31 -0400

    LineManager headerfile implementation

--- project/MS3/LineManager.h
+++ project/MS3/LineManager.h
@@ -5,3 +5,30 @@
 //
 // I confirm that I am the only author of this file
 //   and the content was created entirely by me.
+
+#ifndef SDDS_LINEMANAGER_H
+#define SDDS_LINEMANAGER_H
+
+#include "CustomerOrder.h"
+#include "Workstation.h"
+
+namespace sdds
+{
+    class LineManager
+    {
+        std::vector<Workstation*> m_activeLine;
+        size_t m_cntCustomerOrder{};
+        Workstation* m_firstStation{nullptr};
+    public:
+        LineManager(const std::string& file, const std::vector<Workstation*>& stations);
+
+        // Modifiers
+        void reorderStations();
+        bool run(std::ostream& os);
+
+        // Queries
+        void display(std::ostream& os) const;
+    };
+}
+
+#endif // SDDS_LINEMANAGER_H
